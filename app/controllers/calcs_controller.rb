97bbfcd8ba27ecab2fc0c34ed52e2486cb3c5d6f
Yamamotonasu <deep.blue.deep@outlook.jp>
2019-03-13 15:06:16 +0900

    平日課題3 完了

---
+++ app/controllers/calcs_controller.rb
@@ -0,0 +1,20 @@
+class CalcsController < ApplicationController
+  def show
+    value1 = params[:value1].to_i
+    value2 = params[:value2].to_i
+    operators = params[:operators]
+    
+    @result =  case operators
+              when 'addition'
+                value1 + value2
+              when 'subtraction'
+                value1 - value2
+              when 'multiplication'
+                value1 * value2
+              when 'division'
+                value1 / value2
+              else
+                "正しい値をURLに入力してください。"
+              end
+  end
+end
