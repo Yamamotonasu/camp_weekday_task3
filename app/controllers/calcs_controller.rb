da42a55ed53dcad16e4246573210cfb61de2a998
Yamamotonasu <deep.blue.deep@outlook.jp>
2019-03-13 16:06:06 +0900

    平日課題4 完了

--- app/controllers/calcs_controller.rb
+++ app/controllers/calcs_controller.rb
@@ -1,20 +1,11 @@
 class CalcsController < ApplicationController
+  include Common
+
   def show
     value1 = params[:value1].to_i
     value2 = params[:value2].to_i
     operators = params[:operators]
     
-    @result =  case operators
-              when 'addition'
-                value1 + value2
-              when 'subtraction'
-                value1 - value2
-              when 'multiplication'
-                value1 * value2
-              when 'division'
-                value1 / value2
-              else
-                "正しい値をURLに入力してください。"
-              end
+    @result = calculation(operators, value1, value2)
   end
 end
